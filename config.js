3679fa5b820708f5f18c61c5ea71d42bfcfb57c3
haikalgnz <haikalofc8@gmail.com>
2021-11-10 07:10:16 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 let fs = require('fs')
-global.owner = ['6281351047727'] // Letakan nomor kamu disini
-global.mods = ['6281351047727'] // Moderator?
+global.owner = ['6289696454872'] // Letakan nomor kamu disini
+global.mods = ['6289696454872'] // Moderator?
 global.prems = JSON.parse(fs.readFileSync('./src/premium.json')) // Pengguna premium tidak memerlukan limit
 global.APIs = { // API Prefix
   // nama: 'https://website'
